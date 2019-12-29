044671862a36cd65f7168a228f7fddc4eea5bf23
alk831 <adam54111@gmail.com>
2019-12-29 20:46:04 +0100

    refactor specs & plugin tester util

--- src/use-state.spec.ts
+++ src/use-state.spec.ts
@@ -1,28 +1,47 @@
 import * as babel from '@babel/core';
 import { useStatePlugin } from './use-state';
 
-const mountPluginTester = (...plugins: babel.PluginItem[]) => {
-  return (code: string): string => {
-    const result = babel.transform(code, { plugins });
+const mountPluginTester = (
+  ...plugins: babel.PluginItem[]
+) => (code: string): string => {
+  const result = babel.transform(code, { plugins });
 
-    if (result === null || result.code == null) {
-      throw Error;
-    }
+  if (result?.code == null) {
+    throw new Error(
+      `Could not transform code properly: "${result?.code}"`
+    );
+  }
 
-    return result.code;
-  };
+  return result.code;
 }
 
 describe('useState Plugin', () => {
 
   let pluginTester = mountPluginTester(useStatePlugin);
 
-  it('should transform useState correctly', () => {
-    const result = pluginTester(
-      `const [counter, setCounter] = useState(0);`
-    );
+  describe('state declaration', () => {
 
-    expect(result).toMatchSnapshot();
+    it('should transform destructuring to single variable and change function name', () => {
+      const result = pluginTester(
+        `const [counter, setCounter] = useState(0);`
+      );
+  
+      expect(result).toEqual(`const counter = reactive(0);`);
+    });
+
+  });
+
+
+  describe('state update', () => {
+
+    it('should remove "setState" function call', () => {
+      const result = pluginTester(
+        `setCounter(counter + 1);`
+      );
+
+      expect(result).toEqual(`counter + 1;`);
+    });
+
   });
 
 });
