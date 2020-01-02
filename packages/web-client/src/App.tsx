895a681fc8f4b7f5558bc74d86002b8d0a98b1fc
alk831 <adam54111@gmail.com>
2020-01-02 16:44:44 +0100

    add code editor

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -4,31 +4,45 @@ import logo from './logo.svg';
 import './App.css';
 import * as babelHooksToCompositionPlugin from 'babel-plugin-hooks-to-composition';
 import * as babel from '@babel/core';
+import { split } from 'react-ace';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-textmate';
+
+const SplitEditor = split  as any;
 
 const defaultCode = `
+function useCounter() {
   const [counter, setCounter] = useState(0);
-
-  setCounter(c => c + 1);
+  
+  const increment = () => setCounter(c => c + 1);
+  
+  return { counter, increment };
+}
 `.trim();
 
 export function App() {
-  const [code, setCode] = useState(defaultCode);
+  const [reactCode, setReactCode] = useState(defaultCode);
 
-  const transformedCode = babel.transform(code, {
-    plugins: [babelHooksToCompositionPlugin],
-    retainLines: true
-  });
+  // const transformedCode = babel.transform(code, {
+  //   plugins: [babelHooksToCompositionPlugin],
+  //   retainLines: true
+  // });
 
   return (
     <div className="App">
+      <SplitEditor
+        mode="javascript"
+        splits={2}
+        theme="textmate"
+        fontSize={14}
+        value={[reactCode, reactCode]}
+        onChange={([reactCode]: any) => {
+          setReactCode(reactCode);
+        }}
+        style={{ width: '80vw', height: '80vh', margin: '50px auto' }}
+        enableBasicAutocompletion      
+      />
       <header className="App-header">
-        <textarea
-          value={code}
-          onChange={(e => setCode(e.target.value))}
-        />
-        <pre>
-          {transformedCode}
-        </pre>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
