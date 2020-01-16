943c4809c19677328b6e8399c91a115039917e36
alk831 <adam54111@gmail.com>
2020-01-16 21:01:31 +0100

    cleanup use state visitor

--- packages/babel-plugin-hooks-to-composition/visitors/use-state/index.ts
+++ packages/babel-plugin-hooks-to-composition/visitors/use-state/index.ts
@@ -1,12 +1,4 @@
-import { PluginHandler } from '../../types';
-import { Node } from '../../types';
-import { Identifier, ArrayPattern, Expression } from 'babel-types';
 import * as t from 'babel-types';
-import {
-  VUE_STATE_FUNC_NAME,
-  REACT_STATE_FUNC_NAME,
-  REACT_STATE_SETTER_PREFIX,
-} from '../../consts';
 import { Visitor } from 'babel-traverse';
 import {
   isSetStateCallback,
@@ -17,80 +9,13 @@ import {
   createReactUseRefDeclarator,
 } from '../../helpers';
 
-/** useState(...) */
-const isUseStateFunc = (node: Node<Identifier>): boolean => node.name === REACT_STATE_FUNC_NAME;
-
-/** setCounter */
-const isUseStateSetter = (exp: Expression): exp is Identifier => t.isIdentifier(exp) && exp.name.startsWith(REACT_STATE_SETTER_PREFIX);
-
 interface StateValueName extends String {}
 interface StateSetterName extends String {}
 
-let stateDeclarationsSet = new Set<StateValueName>();
 let stateDeclarationsMap = new Map<StateSetterName, StateValueName>();
 
-export const useStatePlugin: PluginHandler = (babel) => ({
-  visitor: {
-    CallExpression(path) {
-      const { callee, arguments: args } = path.node;
-      const [setStateArg] = args;
-
-      if (isUseStateSetter(callee) && setStateArg) {
-        if (setStateArg.type === 'BinaryExpression') {
-          path.replaceWith(setStateArg);
-        } else if (setStateArg.type === 'ArrowFunctionExpression') {
-          return;
-          // const stateValueName = stateDeclarationsMap.get(callee.name as StateSetterName);
-          // const [stateValueParam] = setStateArg.params;
-          // const isStateSetterDeclared = stateValueName === undefined;
-
-          // if (stateValueName === undefined) return;
-          // if (!t.isIdentifier(stateValueParam)) return;
-
-          // stateValueParam.name = 'abc';
-
-          // path.replaceWith(setStateArg.body);
-        }
-      }
-    },
-    Identifier(path) {
-      if (isUseStateFunc(path.node)) {
-        const vueStateFuncName = t.identifier(VUE_STATE_FUNC_NAME);
-        path.replaceWith(vueStateFuncName);
-      }
-    },
-    ArrayPattern(path) {
-      const { node } = path;
-      const hasTwoElements = node.elements.length === 2;
-      const [stateValue, stateSetter] = node.elements;
-    
-      if (!hasTwoElements) return;
-      if (!t.isIdentifier(stateValue)) return;
-      if (!isUseStateSetter(stateSetter)) return;
-
-      const setterNameByPattern = 
-        REACT_STATE_SETTER_PREFIX +
-        stateValue.name.charAt(0).toUpperCase() +
-        stateValue.name.substring(1);
-
-      const isCorrectSetterName = stateSetter.name === setterNameByPattern;
-
-      if (!isCorrectSetterName) return;
-
-      // track state delcarations
-      stateDeclarationsSet.add(stateValue.name as StateValueName);
-      stateDeclarationsMap.set(
-        stateSetter.name as StateSetterName,
-        stateValue.name as StateValueName
-      );
-
-      const variableIdentifier = t.identifier(stateValue.name);
-      path.replaceWith(variableIdentifier);
-    },
-  }
-});
-
 const replaceUseStateWithReactiveOrRef = (): Visitor => ({
+  
   // [counter, setCounter] = useState(0);
   VariableDeclarator(path) {
     const stateDeclarationInfo = isReactStateDeclarator(path.node);
