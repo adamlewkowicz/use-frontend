60afa277b51bd2ff3afd8c51aa85b53bf73c8a92
alk831 <adam54111@gmail.com>
2020-02-07 15:32:29 +0100

    Updates

---
+++ precache-manifest.891d9ff3cd4b3b1f65851479e6345d61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb2296cad9287a50773f8304ac8f314",
+    "url": "/use-frontend/index.html"
+  },
+  {
+    "revision": "e0dab36c0f8cb04d7012",
+    "url": "/use-frontend/static/css/main.e1047328.chunk.css"
+  },
+  {
+    "revision": "afb2b58f10ecb5a6cef4",
+    "url": "/use-frontend/static/js/2.f4bf8d3f.chunk.js"
+  },
+  {
+    "revision": "ac0602e46a1c5243736e54c9b884d891",
+    "url": "/use-frontend/static/js/2.f4bf8d3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0dab36c0f8cb04d7012",
+    "url": "/use-frontend/static/js/main.f8049e3f.chunk.js"
+  },
+  {
+    "revision": "ac07be14cb4dac3b7beb",
+    "url": "/use-frontend/static/js/runtime-main.0c40ec3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/use-frontend/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9075ee3c6921edb5424e07c5c1ec6328",
+    "url": "/use-frontend/static/media/vue-logo.9075ee3c.svg"
+  }
+]);
